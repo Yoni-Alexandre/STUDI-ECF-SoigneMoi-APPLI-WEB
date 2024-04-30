6c1682b24a2a8180d3017a84a07ee265c949e283
Yoni-Alexandre <yoni-brault@blanche-de-castille.fr>
2024-04-30 12:22:29 +0200

    - Ajout de la vérification du statut de chaque rendez-vous. - Ajout de nouvelles procédures dans le readme.md.

--- readme.md
+++ readme.md
@@ -1440,3 +1440,33 @@ la vue de l' `include`
     </div>
 </div>
 ```
+Ajout de la vérification du statut de chaque rendez-vous avant de les afficher à l'utilisateur
+
+Mise à jour du statut de chaque rendez-vous selon que la date du rendez-vous est "effectué", "en cours", "à venir"
+
+```bash
+        // Création d'un objet DateTime qui contient la date et l'heure actuelle
+        $actuelle = new \DateTime();
+
+        // Boucle qui parcourt chaque rendez-vous dans le tableau $rdvs
+        foreach ($rdvs as $rdv) {
+            // Obtention de la date du rendez-vous actuel et la stocke dans une variable $dateRDV
+            $dateRDV = $rdv->getDate();
+
+            // Vérification si la date du rendez-vous est passée et si le statut du rendez-vous n'est pas déjà "effectué".
+            if ($dateRDV < $actuelle && $rdv->getStatus() != 'effectué') {
+                // Change le statut du rendez-vous à "effectué"
+                $rdv->setStatus('effectué');
+
+              // Vérifie si la date du rendez-vous est la même que la date actuelle et si le statut du rendez-vous n'est pas déjà "en cours"
+            } elseif ($dateRDV == $actuelle && $rdv->getStatus() != 'en cours') {
+                // Change le statut du rendez-vous à "en cours"
+                $rdv->setStatus('en cours');
+            } // sinon le statut reste 'à venir' par défaut
+
+            // Prépare le rendez-vous pour être enregistré (ou mise à jour) dans la base de données.
+            $entityManager->persist($rdv);
+        }
+        // Flush dans la base de données les opérations préparées avec $entityManager->persist($rdv);
+        $entityManager->flush();
+```
--- src/Controller/RendezVousController.php
+++ src/Controller/RendezVousController.php
@@ -16,11 +16,36 @@ use Symfony\Component\Routing\Attribute\Route;
 class RendezVousController extends AbstractController
 {
     #[Route('/compte/rendez-vous', name: 'app_rendez-vous')]
-    public function rdv(Security $security, RendezVousUtilisateurRepository $rendezVousUtilisateurRepository): Response
+    public function rdv(Security $security, RendezVousUtilisateurRepository $rendezVousUtilisateurRepository, EntityManagerInterface $entityManager): Response
     {
         // Utilisation du service de sécurité de Symfony pour obtenir l'utilisateur actuellement connecté
         $utilisateur = $security->getUser();
         $rdvs = $rendezVousUtilisateurRepository->findBy(['utilisateur' => $utilisateur]);
+
+        // Création d'un objet DateTime qui contient la date et l'heure actuelle
+        $actuelle = new \DateTime();
+
+        // Boucle qui parcourt chaque rendez-vous dans le tableau $rdvs.
+        foreach ($rdvs as $rdv) {
+            // Obtention de la date du rendez-vous actuel et la stocke dans la variable $dateRDV
+            $dateRDV = $rdv->getDate();
+
+            // Vérification si la date du rendez-vous est passée et si le statut du rendez-vous n'est pas déjà "effectué".
+            if ($dateRDV < $actuelle && $rdv->getStatus() != 'effectué') {
+                // Change le statut du rendez-vous à "effectué"
+                $rdv->setStatus('effectué');
+
+              // Vérifie si la date du rendez-vous est la même que la date actuelle et si le statut du rendez-vous n'est pas déjà "en cours"
+            } elseif ($dateRDV == $actuelle && $rdv->getStatus() != 'en cours') {
+                // Change le statut du rendez-vous à "en cours"
+                $rdv->setStatus('en cours');
+            } // sinon le statut reste 'à venir' par défaut
+
+            // Prépare le rendez-vous pour être enregistré (ou mise à jour) dans la base de données.
+            $entityManager->persist($rdv);
+        }
+        // Flush dans la base de données les opérations préparées avec $entityManager->persist($rdv);
+        $entityManager->flush();
 
         return $this->render('rendez_vous/rendezVous.html.twig',[
             'rdvs' => $rdvs,
